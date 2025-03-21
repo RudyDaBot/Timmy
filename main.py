e4a08fdc164b1cce7efaabc95f650c731b3d163b
Rudraksh TIku <72981271+RudyDaBot@users.noreply.github.com>
2025-03-21 22:50:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
-from ollama import chat
-from ollama import ChatResponse
+import core
 import os
+import speech_recognition as sr
+import pyttsx3
+import time
+import json
 
-contextual = ""
+debugMode = bool(input("Enter Debug Mode? : "))
 
-def askOllama (context, inputQuery) :
-    response: ChatResponse = chat(model='llama3.2', messages=[
-  {
-    'role': 'assistant',
-    'content': context
-  },{
-    'role': 'user',
-    'content': inputQuery,
-  },
-])
-    return response.message.content
+with open('config.json', 'r') as file: 
+  config = json.load(file)
+  if debugMode : 
+    print(config)
 
+
+contextual = config['initialContext']
+print("contexttual is this ", contextual)
+
+r = sr.Recognizer()
+ss = pyttsx3.init()
+ss.setProperty('rate', 130)
 
 print("Meta Llama 3.2 Internet Access Demo. Type /bye to quit, ctrl+c may not cleanly flush occupied memory")
 
-while(True) :
-  print(">>>", end="")
-  userInput=str(input(""))
+modeOfInput = str(input("Enter mode of input, voice or text : "))
 
-  if userInput == "/bye" :
+
+if modeOfInput == "text" : 
+  print("Text Mode Active. Restart TIMMY for voice mode.")
+
+  while(True) :
+    print(">>>", end="")
+    userInput=str(input(""))
+
+    if userInput == "/bye" :
      os.system("ollama stop llama3.2")
+     print("Exiting and flushing memory.")
      break
-  elif userInput == "/Wipe-Context" :
+    elif userInput == "/Wipe-Context" :
      os.system("clear")
      contextual = ""
      print('Context Wiped. Reinitialized. The model has now started a new conversation.')
-  else :
-    tempDump = askOllama(contextual, userInput)
-    print(tempDump)
-    contextual += tempDump
- 
+    elif userInput == "/k":
+      pass
+    else :
+      print("Thinking...")
+      tempDump = core.askOllamaText(contextual, userInput)
+      print(tempDump)
+      contextual += tempDump
+
+elif modeOfInput == "voice" : 
+
+  print("Voice Mode active. to exit, say (exit). ")
+
+  while(True) : 
+
+    
+      print("Ready.")
+
+      print("Voice Mode Active. Type 'ready' to begin.")
+      modeTemp = str(input(">>>")) 
+
+      if modeTemp == "ready" :
+
+        try:
+          with sr.Microphone() as source: 
+            audio = r.listen(source)
+            print(audio)
+            text=r.recognize_google(audio)
+            if debugMode : 
+              print("Heard -> ", text)
+        except:
+          print("Error detected. Either your internet or your mic/way of speaking failed.")
+          continue
+      
+      elif modeTemp == "exit" : 
+        print("Exiting and flushing memory.")
+        os.system("ollama stop llama3.2")
+        break
+
+      else :
+        print("Thinking...")
+        tempDumpVoice = core.askOllamaText(contextual, text)
+        print(tempDumpVoice)
+        ss.say(tempDumpVoice)
+        ss.runAndWait()
+        contextual += tempDumpVoice
+        time.sleep(15)
 
